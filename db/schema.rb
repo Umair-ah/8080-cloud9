1a11d5762ab9c1cf731b544ad1f92a0dcef494da
Ubuntu <ubuntu@ip-172-31-38-144.ap-south-1.compute.internal>
2022-11-08 13:43:02 +0000

    Added user profile tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_163027) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_134026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_163027) do
   create_table "plans", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "job_title"
+    t.integer "phone_number"
+    t.string "contact_email"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
