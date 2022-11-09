d74aae0ab1cd6adbfb39808860f44eabdabffa2d
Ubuntu <ubuntu@ip-172-31-38-144.ap-south-1.compute.internal>
2022-11-09 17:28:18 +0000

    Added form page for creating profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   # Get to /users/:user_id/profile/new
   def new
-  # Render blank profile details form
+    # Render blank profile details form
+    @profile = Profile.new
   end
 end
